d76c3037f39a54cac03de11c99afb1ccca52c6fe
Diego-Hiriart <hiriart.leon.d@gmail.com>
2023-03-06 02:19:37 -0500

    Serial read Arduino prototype

---
+++ Code/Machine/NodeMCUToArduino-CommTest/NodeMCUToArduino-CommTest.ino
@@ -0,0 +1,49 @@
+const int leftLed=2;
+const int rightLed=3;
+int leftTouch=0;
+int rightTouch=0;
+const String messageStartDelimiter = "s";
+const String messageEndDelimiter = "\n";
+const int messageLength = 8;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(leftLed, OUTPUT);
+  pinMode(rightLed, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  serialReceive();
+  ledControl();
+}
+
+void serialReceive(){
+  //Read string like start;left;right;end e.g. s;0;1;e
+  if(Serial.available() >= 8){
+    String nodeMCUMessage = "";
+    while(!nodeMCUMessage.endsWith(messageEndDelimiter)){
+      nodeMCUMessage += (char)Serial.read();
+    }
+    Serial.write(nodeMCUMessage.c_str());
+    if(nodeMCUMessage.startsWith(messageStartDelimiter) && nodeMCUMessage.length() == messageLength){
+      String leftValue = nodeMCUMessage.substring(2, 3);
+      String rightValue = nodeMCUMessage.substring(4, 5);
+      leftTouch=leftValue.toInt();
+      rightTouch=rightValue.toInt();
+      String readValues = "left: "+String(leftTouch)+", right: "+String(rightTouch)+"\n";
+      Serial.write(readValues.c_str());
+  }
+  }
+}
+
+void ledControl(){
+  digitalWrite(leftLed, leftTouch);
+  digitalWrite(rightLed, rightTouch);
+  delay(1000);
+  leftTouch=0;
+  rightTouch=0;
+  digitalWrite(leftLed, leftTouch);
+  digitalWrite(rightLed, rightTouch);
+}
---
+++ Code/Machine/RightLeft-NodeMCUInputTest/RightLeft-NodeMCUInputTest.ino
@@ -0,0 +1,38 @@
+const int inputLeft=2;
+const int inputRight=3;
+int leftTouch=0;
+int rightTouch=0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(inputLeft, INPUT);
+  pinMode(inputRight, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  fencerTouch();
+  serialSend();
+}
+
+void fencerTouch(){
+  //Check if a touch was made
+  int leftTouch = digitalRead(inputLeft);
+  int rightTouch = digitalRead(inputRight);  
+}
+
+void serialSend(){
+  //Send latest data
+  String message = "s;";//Follow this format: start;leftFencer;rightFencer;end e.g. s;1;0;e
+  message += String(leftTouch)+";";
+  message += String(rightTouch)+";";
+  message += "e";
+  message += "\n";
+  int charMessageLength = message.length()+1;
+  char serialMessage[charMessageLength];
+  message.toCharArray(serialMessage, charMessageLength);
+  Serial.write(serialMessage);
+  leftTouch=0;
+  rightTouch=0;  
+}
