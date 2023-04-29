f0bf4353d47d5c6ccdb8b1657657edc04b459015
ChristianSama <christiansama1601@gmail.com>
2023-04-29 00:40:45 -0500

    chore: add db seeding

---
+++ Code/Web_app/server/prisma/seed.ts
@@ -0,0 +1,41 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+const users = [
+  {
+    email: "pestrella@gmail.com",
+    password: "password123",
+    names: "Patricio",
+    lastNames: "Estrella",
+    roles: ["student"],
+  },
+  {
+    email: "mscott@gmail.com",
+    password: "password123",
+    names: "Michael",
+    lastNames: "Scott",
+    roles: ["trainer"],
+  },
+  {
+    email: "rsanchez@gmail.com",
+    password: "password123",
+    names: "Rick",
+    lastNames: "Sanchez",
+    roles: ["admin"],
+  },
+];
+
+async function main() {
+  await prisma.user.createMany({
+    data: users,
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
