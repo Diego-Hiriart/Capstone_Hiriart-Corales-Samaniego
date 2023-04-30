f3460948a93b391e0600ac459a129148bf568d1b
Luis Corales <lcorales@stackbuilders.com>
2023-04-30 15:00:10 -0500

    all cruds

---
+++ Code/Web_app/server/src/data/registrationLink.ts
@@ -0,0 +1,81 @@
+import { PrismaClient, RegistrationLink } from "@prisma/client";
+
+import { errorLog } from "../utils/logs";
+
+const prisma = new PrismaClient();
+
+export async function findRegistrationLinkById(id: number) {
+  try {
+    const registrationLink = await prisma.registrationLink.findUnique({
+      where: {
+        registrationLinkID: id,
+      },
+    });
+    return registrationLink;
+  } catch (error) {
+    errorLog(error);
+    return undefined;
+  }
+}
+
+export async function findAllRegistrationLink() {
+  try {
+    const registrationLink = await prisma.registrationLink.findMany();
+    return registrationLink;
+  } catch (error) {
+    errorLog(error);
+    return [];
+  }
+}
+
+export async function createRegistrationLink(data: RegistrationLink) {
+  try {
+    const registrationLink = await prisma.registrationLink.create({
+      data: {
+        fencerID: data.fencerID,
+        expirationDate: data.expirationDate,
+        valid: data.valid,
+      },
+    });
+    return registrationLink;
+  } catch (error) {
+    errorLog(error);
+    return undefined;
+  }
+}
+
+export async function updateRegistrationLinkById(
+  id: number,
+  data: RegistrationLink
+) {
+  try {
+    const registrationLink = await prisma.registrationLink.update({
+      where: {
+        registrationLinkID: id,
+      },
+      data: {
+        fencerID: data.fencerID || undefined,
+        expirationDate: data.expirationDate || undefined,
+        valid: data.valid || undefined,
+      },
+    });
+    return registrationLink;
+  } catch (error) {
+    errorLog(error);
+    return undefined;
+  }
+}
+
+export async function deleteRegistrationLinkById(id: number) {
+  try {
+    const registrationLink = await prisma.registrationLink.delete({
+      where: {
+        registrationLinkID: id,
+      },
+    });
+    return registrationLink;
+  } catch (error) {
+    errorLog(error);
+    return undefined;
+  }
+}
---
+++ Code/Web_app/server/src/data/trainingCombat.ts
@@ -0,0 +1,87 @@
+import { PrismaClient, TrainingCombat } from "@prisma/client";
+
+import { errorLog } from "../utils/logs";
+
+const prisma = new PrismaClient();
+
+export async function findTrainingCombatById(id: number) {
+  try {
+    const trainingCombat = await prisma.trainingCombat.findUnique({
+      where: {
+        trainingCombatID: id,
+      },
+    });
+    return trainingCombat;
+  } catch (error) {
+    errorLog(error);
+    return undefined;
+  }
+}
+
+export async function findAllTrainingCombat() {
+  try {
+    const trainingCombat = await prisma.trainingCombat.findMany();
+    return trainingCombat;
+  } catch (error) {
+    errorLog(error);
+    return [];
+  }
+}
+
+export async function createTrainingCombat(data: TrainingCombat) {
+  try {
+    const trainingCombat = await prisma.trainingCombat.create({
+      data: {
+        fencer1ID: data.fencer1ID,
+        fencer2ID: data.fencer2ID,
+        fencer1Score: data.fencer1Score,
+        fencer2Score: data.fencer2Score,
+        dateTime: data.dateTime,
+        winnerFencerID: data.winnerFencerID,
+      },
+    });
+    return trainingCombat;
+  } catch (error) {
+    errorLog(error);
+    return undefined;
+  }
+}
+
+export async function updateTrainingCombatById(
+  id: number,
+  data: TrainingCombat
+) {
+  try {
+    const trainingCombat = await prisma.trainingCombat.update({
+      where: {
+        trainingCombatID: id,
+      },
+      data: {
+        fencer1ID: data.fencer1ID || undefined,
+        fencer2ID: data.fencer2ID || undefined,
+        fencer1Score: data.fencer1Score || undefined,
+        fencer2Score: data.fencer2Score || undefined,
+        dateTime: data.dateTime || undefined,
+        winnerFencerID: data.winnerFencerID || undefined,
+      },
+    });
+    return trainingCombat;
+  } catch (error) {
+    errorLog(error);
+    return undefined;
+  }
+}
+
+export async function deleteTrainingCombatById(id: number) {
+  try {
+    const trainingCombat = await prisma.trainingCombat.delete({
+      where: {
+        trainingCombatID: id,
+      },
+    });
+    return trainingCombat;
+  } catch (error) {
+    errorLog(error);
+    return undefined;
+  }
+}
