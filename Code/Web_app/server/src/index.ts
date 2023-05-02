6407f3e624fdab4da83cab41dff48ebebc64b6ab
Christian Samaniego <christiansama1601@gmail.com>
2023-05-02 06:39:10 +0000

    configure devcontainer to use Codespaces

--- Code/Web_app/server/src/index.ts
+++ Code/Web_app/server/src/index.ts
@@ -8,10 +8,9 @@ import { debugLog, serverLog } from "./utils/logs";
 
 const app = express();
 const port = process.env.PORT || 3000;
-const client_port = process.env.CLIENT_PORT || 5173;
 
 app.use(cors({
-  origin: `http://localhost:${client_port}`,
+  origin: process.env.CLIENT_URL,
   credentials: true,
 }));
 app.use(express.json());
