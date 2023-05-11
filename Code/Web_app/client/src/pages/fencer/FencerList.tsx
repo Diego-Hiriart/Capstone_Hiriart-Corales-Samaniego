dfde30e689e8d2dc2492674b0f7ca21c1cd7f6be
ChristianSama <christiansama1601@gmail.com>
2023-05-11 01:40:01 -0500

    add fencer list

--- Code/Web_app/client/src/pages/fencer/FencerList.tsx
+++ Code/Web_app/client/src/pages/fencer/FencerList.tsx
@@ -1,7 +1,71 @@
-const FencerList = () => {
-  return (
-    <div>FencerList</div>
-  )
-}
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemButton,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+import axios from "../../services/axios";
+import { Link as RouterLink } from "react-router-dom";
+import { Fencer } from "../../types";
 
-export default FencerList
+const FencerList = () => {
+  const [fencers, setFencers] = useState<Fencer[]>(null!);
+
+  useEffect(() => {
+    const fetchFencers = async () => {
+      const { data } = await axios.get("/dashboard/fencer");
+      setFencers(data.data);
+    };
+    fetchFencers();
+  }, []);
+
+  return (
+    <Container component="main" maxWidth="sm">
+      <Box py={{ xs: 2, lg: 4 }}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            flexWrap: "wrap",
+          }}
+        >
+          <Typography variant="h1" alignSelf="start">
+            Fencers
+          </Typography>
+          <Button component={RouterLink} to="create" variant="contained">
+            Crear nuevo
+          </Button>
+        </Box>
+        <List sx={{ mt: 1 }}>
+          {fencers?.map((fencer) => (
+            <ListItem key={fencer.fencerID} disablePadding>
+              <ListItemButton
+                sx={{ px: 1 }}
+                component={RouterLink}
+                to={String(fencer.fencerID)}
+              >
+                <ListItemAvatar>
+                  <Avatar></Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary={`${fencer.user.names} ${fencer.user.lastNames}`}
+                />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        {/* TODO: Add pagination */}
+      </Box>
+    </Container>
+  );
+};
+
+export default FencerList;
--- Code/Web_app/client/src/types.ts
+++ Code/Web_app/client/src/types.ts
@@ -31,3 +31,34 @@ export interface Trainer {
   pictureURL: string;
   user: User;
 }
+
+export type Fencer = {
+  fencerID: number
+  userID: number | null
+  trainingGroupID: number
+  idNumber: string
+  emergencyPhone: string
+  birthDate: Date
+  bloodType: string
+  sex: string
+  laterality: string
+  phone: string
+  insurance: string
+  inscriptionDate: Date
+  startDate: Date
+  occupation: string
+  schedule: string
+  legalGuardian: string
+  leadSource: string
+  inscriptionReason: string
+  height: number
+  weight: number
+  physicalActivity: string
+  medicalFamily: string
+  medicalPersonal: string
+  personalMedicalDetails: string
+  weapon: string
+  pictureURL: string
+  guestName: string | null
+  user: User;
+}
