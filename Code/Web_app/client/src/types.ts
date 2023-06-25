14d393494c8ce59a23df7af2d1d64787a1c11b17
Luis Corales <luisaugustocorales@hotmail.com>
2023-06-24 23:51:38 -0500

    add machine combat data logic TODO rebase

--- Code/Web_app/client/src/types.ts
+++ Code/Web_app/client/src/types.ts
@@ -1,4 +1,11 @@
-import { boolean } from "zod";
+export interface MachineCombatData {
+  machineName: string;
+  leftScore: number;
+  rightScore: number;
+  dateTime: Date;
+  leftPriority: boolean;
+  rightPriority: boolean;
+}
 
 export interface LoginFormInputs {
   email: string;
