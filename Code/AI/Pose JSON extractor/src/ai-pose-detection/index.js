8773e6c19e33676391483a2ed84add1248d5043a
Diego-Hiriart <hiriart.leon.d@gmail.com>
2023-06-27 02:39:49 -0500

    feat: add 2D extraction option

--- Code/AI/Pose JSON extractor/src/ai-pose-detection/index.js
+++ Code/AI/Pose JSON extractor/src/ai-pose-detection/index.js
@@ -16,6 +16,7 @@ let rafId;
 let posesPacketSize = 0,
   videoDuration,
   videoDone;
+let extract2D;
 let lastVideoTime;
 //To save poses JSONs in packets
 let posesJSON;
@@ -42,7 +43,8 @@ const savePoseData = (poseData) => {
     poseData[0] !== undefined &&
     poseData[0] !== null
   ) {
-    let keypoints3DData = poseData[0]['keypoints3D'];
+    let extrationKeypoints = extract2D ? 'keypoints' : 'keypoints3D';
+    let keypoints3DData = poseData[0][extrationKeypoints];
     //Check if the current packet in the JSON is full, create a new one if so, otherwise add pose data
     if (posesJSON[currentPosesIndex].length == posesPacketSize) {
       posesJSON.push([keypoints3DData]);
@@ -93,10 +95,9 @@ const downloadPosesJSON = () => {
     });
     a.href = URL.createObjectURL(file);
     let fileName = videoName;
-    fileName = `${fileName.substring(
-      0,
-      fileName.lastIndexOf('.')
-    )}_poses-JSON.json`;
+    fileName = `${fileName.substring(0, fileName.lastIndexOf('.'))}_${
+      extract2D ? '2D' : '3D'
+    }poses-JSON.json`;
     a.download = fileName;
     a.click();
     a.remove();
@@ -185,9 +186,11 @@ export const poseDetectionAI = async (extractionData) => {
   videoName = extractionData.videoFile.name;
   //Update poses packet size
   posesPacketSize = Number(extractionData.posesPacketSize);
-  //Ensure video can be divided in the packet size +1 (to ensure initial empty detection doesnt cause failure obtaining packet size)
+  //Whether to extract 2D keypoints or not (default is 3d)
+  extract2D = extractionData.extract2D;
+  //Ensure video can be divided in the packet size +2 (to ensure initial empty detection doesnt cause failure obtaining packet size)
   if ((videoDuration / posesPacketSize) * posesPacketSize > videoDuration) {
-    posesPacketSize += 1;
+    posesPacketSize += 2;
   }
   //Load and start video
   camera = new Context();
