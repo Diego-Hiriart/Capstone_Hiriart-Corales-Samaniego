1d5e5a276a7cf3f40c15e873ec41dd81b5041820
Diego-Hiriart <hiriart.leon.d@gmail.com>
2023-07-01 15:38:17 -0500

    refactor: unused routes

--- Code/AI/AI trainer/src/controllers/ai_controller.ts
+++ Code/AI/AI trainer/src/controllers/ai_controller.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express';
 import { History } from '@tensorflow/tfjs';
-const multer = require('multer');
 
 import { errorLog } from '../utils/logs';
 import { createAndTrainModel } from '../errors_AI/errorAnalysisModel';
@@ -15,15 +14,4 @@ export async function trainModel(req: Request, res: Response) {
     req.body.posesPerMovement
   );
   return res.status(200).json(trainingResult);
-}
-
-/*To save trained model, POST */
-export async function saveModel(req: Request, res: Response) {
-  try {
-    const upload = multer({ dest: '../errors_AI_model_data' });
-    console.log(req);
-    return res.status(200);
-  } catch (saveError) {
-    return res.status(500).json(saveError);
-  }
 }
