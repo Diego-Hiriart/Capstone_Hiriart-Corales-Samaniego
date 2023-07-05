b6c67fe3d02549d5565cf832ee5a913d478efb5f
ChristianSama <christiansama1601@gmail.com>
2023-07-05 01:55:42 -0500

    change password fencer profile

---
+++ Code/Web_app/client/src/components/Dialog/ChangePasswordDialog.tsx
@@ -0,0 +1,106 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField,
+} from "@mui/material";
+import { useForm } from "react-hook-form";
+import { setErrorMap, z } from "zod";
+import useAuth from "../../hooks/useAuth";
+import axios from "../../services/axios";
+import { isAxiosError } from "axios";
+import { useAlert } from "../../hooks/useAlert";
+
+interface Props {
+  open: boolean;
+  handleClose: () => void;
+}
+
+const schema = z.object({
+  currentPassword: z.string().nonempty({ message: "Campo requerido" }),
+  newPassword: z.string().nonempty({ message: "Campo requerido" }),
+});
+
+type ChangePasswordDialogType = z.infer<typeof schema>;
+
+const ChangePasswordDialog = ({ open, handleClose }: Props) => {
+  const { user } = useAuth();
+  const { showError, showSuccess } = useAlert();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+    reset,
+  } = useForm<ChangePasswordDialogType>({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit = async (formData: ChangePasswordDialogType) => {
+    try {
+      await axios.put(`/dashboard/user/${user?.userID}/change-password`, {
+        email: user?.email,
+        data: formData,
+      });
+      showSuccess("Contraseña cambiada correctamente");
+      reset();
+      handleClose();
+    } catch (error) {
+      if (isAxiosError(error)) {
+        if (error.response?.status === 401) {
+          setError("currentPassword", {
+            type: "manual",
+            message: "Contraseña incorrecta",
+          });
+          return;
+        }
+      }
+      showError("Hubo un error al cambiar la contraseña");
+    }
+  };
+
+  return (
+    <Dialog open={open} onClose={handleClose} fullWidth>
+      <DialogTitle>Cambiar Contraseña</DialogTitle>
+      <DialogContent>
+        <Box component="form" noValidate onSubmit={handleSubmit(onSubmit)}>
+          <TextField
+            {...register("currentPassword")}
+            fullWidth
+            margin="normal"
+            id="currentPassword"
+            label="Contraseña Actual"
+            type="password"
+            autoFocus
+            error={!!errors.currentPassword}
+            helperText={errors.currentPassword?.message}
+          />
+          <TextField
+            {...register("newPassword")}
+            fullWidth
+            margin="normal"
+            id="newPassword"
+            label="Nueva Contraseña"
+            type="password"
+            error={!!errors.newPassword}
+            helperText={errors.newPassword?.message}
+          />
+          <DialogActions sx={{ mt: 3 }}>
+            <Button fullWidth variant="outlined" onClick={handleClose}>
+              Cancelar
+            </Button>
+            <Button fullWidth variant="contained" type="submit">
+              Confirmar
+            </Button>
+          </DialogActions>
+        </Box>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default ChangePasswordDialog;
