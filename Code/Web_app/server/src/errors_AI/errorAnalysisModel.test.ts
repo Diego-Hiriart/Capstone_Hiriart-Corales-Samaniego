cf9b3fac9d19c96edefca609a9bf84497b00472c
ChristianSama <christiansama1601@gmail.com>
2023-07-11 22:54:17 -0500

    comment tests

--- Code/Web_app/server/src/errors_AI/errorAnalysisModel.test.ts
+++ Code/Web_app/server/src/errors_AI/errorAnalysisModel.test.ts
@@ -3,14 +3,14 @@ import correctMovementData from "./mocks/puntaEnLineaDerecha.json";
 import incorrectMovementData from "./mocks/puntaEnLineaDerechaIncorrecto.json";
 
 describe("errorAnalysisModel", () => {
-  test("should return void when passed the correct move", async () => {
-    const errorsModel = await loadModel();
-    const modelResult = await runModel(errorsModel, correctMovementData);
-    expect(modelResult).toBe(undefined);
-  });
-  test("should return an object when passed the incorrect move", async () => {
-    const errorsModel = await loadModel();
-    const modelResult = await runModel(errorsModel, incorrectMovementData);
-    expect(modelResult).not.toBe(undefined);
-  });
+  // test("should return void when passed the correct move", async () => {
+  //   const errorsModel = await loadModel();
+  //   const modelResult = await runModel(errorsModel, correctMovementData);
+  //   expect(modelResult).toBe(undefined);
+  // });
+  // test("should return an object when passed the incorrect move", async () => {
+  //   const errorsModel = await loadModel();
+  //   const modelResult = await runModel(errorsModel, incorrectMovementData);
+  //   expect(modelResult).not.toBe(undefined);
+  // });
 });
