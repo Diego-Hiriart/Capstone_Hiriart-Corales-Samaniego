c88f9264b5671216ec5190d22d937d7e7be41249
ChristianSama <christiansama1601@gmail.com>
2023-07-13 22:19:26 -0500

    update password validation

--- Code/Web_app/client/src/pages/fencer/validations/SignupFormValidation.ts
+++ Code/Web_app/client/src/pages/fencer/validations/SignupFormValidation.ts
@@ -5,7 +5,13 @@ export const schema = z
     names: z.string().trim().nonempty({ message: "Campo requerido" }),
     lastNames: z.string().trim().nonempty({ message: "Campo requerido" }),
     email: z.string().email({ message: "Correo inválido" }),
-    password: z.string().min(8, { message: "Contraseña inválida" }),
+    password: z
+      .string()
+      .min(8, { message: "Contraseña debe contener mínimo 8 caractéres" })
+      .max(50, { message: "Contraseña debe contener máximo 50 caractéres" })
+      .regex(/[0-9]/, {
+        message: "Contraseña debe contener al menos un número",
+      }),
     confirmPassword: z.string().min(8, { message: "Contraseña inválida" }),
   })
   .refine((data) => data.password === data.confirmPassword, {
