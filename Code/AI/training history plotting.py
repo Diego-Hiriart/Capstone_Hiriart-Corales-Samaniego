268b31ce463d4039564133b087c943e0bbd0de88
Diego-Hiriart <hiriart.leon.d@gmail.com>
2023-07-15 00:59:56 -0500

    feat: training history plots

--- Code/AI/training history plotting.py
+++ Code/AI/training history plotting.py
@@ -2,7 +2,9 @@ import pandas as pd
 import json as json
 import matplotlib.pyplot as plt
 
-file = open("D:/UDLA/Capstone Project/Repo/Code/AI/errors_AI_model_data/training-history-1000 epochs.json", 'r')
+file = open("D:/UDLA/Capstone Project/Repo/Code/AI/errors_AI_model_data/training-history 10 aug mlp.json", 'r')
 history = json.loads(file.read())
 
-plt.plot(history["epoch"], history["history"]["val_loss"])
+plt.plot(history["epoch"], history["history"]["val_acc"], label="accuracy validación")
+plt.legend()
+plt.show()
